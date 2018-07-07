e3625a6f96150f12c0535f6757f797d6c0457ec4
Oche Ejembi <oche_ejembi@yahoo.com>
2018-07-07 19:04:45 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-service mysql start
-mysql -uroot -e "CREATE DATABASE openmrs"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'root'@'%'"
-mysql -uroot -e "UPDATE mysql.user SET Password=PASSWORD('test') WHERE User='root'"
-mysql -uroot -ptest -e "FLUSH PRIVILEGES"
+
+# service mysql start
+# mysql -uroot -e "CREATE DATABASE openmrs"
+# mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'root'@'%'"
+# mysql -uroot -e "UPDATE mysql.user SET Password=PASSWORD('test') WHERE User='root'"
+# mysql -uroot -ptest -e "FLUSH PRIVILEGES"
+
 /etc/init.d/tomcat7 start
 
 # The container will run as long as the script is running, that's why
