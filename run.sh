0bbc7a94ff7fd5818cae665210674daa26df587e
Oche Ejembi <oche_ejembi@yahoo.com>
2018-07-07 19:29:46 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@
 
 # The container will run as long as the script is running, that's why
 # we need something long-lived here
-exec tail -f /var/log/tomcat7/catalina.out
+exec tail -f /usr/local/tomcat/logs/catalina.out
